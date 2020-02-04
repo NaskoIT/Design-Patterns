9a6bc7597d72e429731d03be63418c4202f69fb8
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-04 19:12:11 +0200

    Proxy pattern

---
+++ StructuralPatterns/ProxyPattern/Math.cs
@@ -0,0 +1,16 @@
+﻿namespace ProxyPattern
+{
+    /// <summary>
+    ///  The 'Real Subject' class
+    /// </summary>
+    public class Math : IMath
+    {
+        public double Add(double x, double y) => x + y;
+
+        public double Sub(double x, double y) => x - y;
+
+        public double Multiply(double x, double y) => x * y;
+     
+        public double Divide(double x, double y) => x / y;
+    }
+}
---
+++ StructuralPatterns/ProxyPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathProxy proxy = new MathProxy();
+            Console.WriteLine($"4 + 2 = {proxy.Add(4, 2)}");
+            Console.WriteLine($"4 - 2 = {proxy.Sub(4, 2)}");
+            Console.WriteLine($"4 * 2 = {proxy.Multiply(4, 2)}");
+            Console.WriteLine($"4 / 2 = {proxy.Divide(4, 2)}");
+
+            try
+            {
+                Console.WriteLine($"4 / 0 = {proxy.Divide(4, 0)}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
