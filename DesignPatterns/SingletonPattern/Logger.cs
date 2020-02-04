9c0dfd59aec95d998db8757dea332e581dedc02e
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-04 12:05:20 +0200

    Singleton pattern two different implementations

---
+++ DesignPatterns/SingletonPattern/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public static class Logger
+    {
+        static Logger()
+        {
+            Instance = new InnerLogger();
+        }
+
+        public static InnerLogger Instance { get; private set; }
+
+        public class InnerLogger
+        {
+            internal InnerLogger()
+            {
+            }
+
+            public void Log(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
