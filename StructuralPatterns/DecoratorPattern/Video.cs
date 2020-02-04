611518c87a603821c04afcd88bedaaf5384e3a16
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-04 20:12:56 +0200

    Decorator pattern

---
+++ StructuralPatterns/DecoratorPattern/Video.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DecoratorPattern
+{
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+    internal class Video : LibraryItem
+    {
+        private readonly string director;
+        private readonly string title;
+        private readonly int playTime;
+
+        public Video(string director, string title, int playTime, int copiesCount)
+        {
+            this.director = director;
+            this.title = title;
+            this.playTime = playTime;
+            CopiesCount = copiesCount;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("\nVideo ---------- ");
+            Console.WriteLine($" Director: {director}");
+            Console.WriteLine($" Title: {title}");
+            Console.WriteLine($" # Copies: {CopiesCount}");
+            Console.WriteLine($" PlayTime: {playTime}");
+        }
+    }
+}
