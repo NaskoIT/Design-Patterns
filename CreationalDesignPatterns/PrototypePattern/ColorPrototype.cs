6f4f822f9be75e4bdb04230af7bdd840f947660a
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-04 15:46:41 +0200

    Prototype Pattern

---
+++ CreationalDesignPatterns/PrototypePattern/ColorPrototype.cs
@@ -0,0 +1,10 @@
+﻿namespace PrototypePattern
+{
+    /// <summary>
+    ///  The 'Prototype' abstract class
+    /// </summary>
+    public abstract class ColorPrototype
+    {
+        public abstract ColorPrototype Clone();
+    }
+}
