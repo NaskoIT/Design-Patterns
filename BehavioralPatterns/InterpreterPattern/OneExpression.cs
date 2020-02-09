a851bdd28778bfc5f64353b8f6f0cadaad31f263
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-09 22:44:06 +0200

    Interpreter pattern

---
+++ BehavioralPatterns/InterpreterPattern/OneExpression.cs
@@ -0,0 +1,18 @@
+﻿namespace InterpreterPattern
+{
+    /// <summary>
+    /// The 'Concrete' expression class
+    /// </summary>
+    public class OneExpression : Expression
+    {
+        public override string One() => "I";
+        
+        public override string Four() => "IV";
+        
+        public override string Five() => "V";
+
+        public override string Nine() => "IX";
+
+        public override int Multiplier() => 1;
+    }
+}
