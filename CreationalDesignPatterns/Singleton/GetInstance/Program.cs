9fc605fcb0352948e07474ee3c2597e16deed43c
Philip Shishov <philshishov@gmail.com>
2020-11-28 22:56:08 +0100

    Add a Creational Design Singleton example

---
+++ CreationalDesignPatterns/Singleton/GetInstance/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GetInstance
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.GetInstance();
+            Singleton singleton1 = Singleton.GetInstance();
+
+            Console.WriteLine(singleton == singleton1);
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ CreationalDesignPatterns/Singleton/GetInstance/Singleton.cs
@@ -0,0 +1,23 @@
+﻿namespace GetInstance
+{
+  public class Singleton
+  {
+    // eager loading
+    //private readonly Singleton instance = new Singleton();
+    private static Singleton instance;
+
+    private Singleton()
+    {
+    }
+
+    public static Singleton GetInstance()
+    {
+      if (instance == null)
+      {
+        instance = new Singleton();
+      }
+
+      return instance;
+    }
+  }
+}
