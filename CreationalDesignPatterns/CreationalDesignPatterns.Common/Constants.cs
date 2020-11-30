6c11e702df3aa8fdaee3e772e9b254b2f5e3caec
Philip Shishov <philshishov@gmail.com>
2020-12-01 00:00:36 +0100

    Common project for storing constants and less code reuse

---
+++ CreationalDesignPatterns/CreationalDesignPatterns.Common/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace CreationalDesignPatterns.Common
+{
+    public class Constants
+    {
+        public const string DisplayMacchiato = "Macchiato";
+        public const string DisplayRegularCoffee = "Regular Coffee";
+        public const string DisplayCappuccino = "Cappuccino";
+        public const string DisplayDoubleCoffee = "Double Coffee";
+
+
+        public const int ContentZeroMl = 0;
+        public const int ContentHundredMl = 100;
+        public const int ContentTwoHundredMl = 200;
+    }
+}
---
+++ CreationalDesignPatterns/CreationalDesignPatterns.Common/InfoMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace CreationalDesignPatterns.Common
+{
+    public class InfoMessages
+    {
+        public const string CoffeeFactoryPrintMessage = "{0} contains {1} ml coffee and {2} ml milk";
+    }
+}
