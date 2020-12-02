03c349dffd57ab78ccab85a0a605e7a3398ed283
Philip Shishov <philshishov@gmail.com>
2020-12-02 21:16:23 +0100

    Add common project for structural patterns

---
+++ StructuralPatterns/StructuralPatterns.Common/Constants.cs
@@ -0,0 +1,36 @@
+﻿namespace StructuralPatterns.Common
+{
+    public class Constants
+    {
+        public const string DisplayChocolateCake = "Chocolate Cake";
+        public const string DisplayCheeseCake = "Cheese Cake";
+        public const string DisplayIceCream = "Ice Cream";
+
+        public const string DisplayWater = "Water";
+        public const string DisplayBenzene = "Benzene";
+        public const string DisplayEthanol = "Ethanol";
+
+        public const string DisplayWaterCompound = "water";
+        public const string DisplayBenzeneCompound = "benzene";
+        public const string DisplayEthanolCompound = "ethanol";
+
+        public const string DisplayCompound = "Compound";
+        public const string DisplayFormula = "Formula";
+        public const string DisplayWeight = "Weight";
+        public const string DisplayMeltingPoint = "Melting Point";
+        public const string DisplayBoilingPoint = "Boiling Point";
+
+        public const string SignMeltingPoint = "M";
+        public const string SignBoilingPoint = "B";
+
+        public const string FaqTitle = "The Bridge Pattern";
+        public const string FaqQuestionOne = "What is it?";
+        public const string FaqQuestionOneAnswer = "A design pattern.";
+        public const string FaqQuestionTwo = "When do we use it?";
+        public const string FaqQuestionTwoAnswer = "When you need to separate the abstraction from an implementation.";
+
+        public const string BookAuthor = "John Sonmez";
+        public const string BookTitle = "The Complete Software Developer’s Career Guide";
+        public const string BookText = "What do I actually need to know to be a software developer?";
+    }
+}
---
+++ StructuralPatterns/StructuralPatterns.Common/PrintMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace StructuralPatterns.Common
+{
+    public class PrintMessages
+    {
+        public const string CafeteriaPrintMessage = "{0} costs {1}$";
+        public const string PickNumbersPrintMessage = "Pick two numbers:";
+    }
+}
